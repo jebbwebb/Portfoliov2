d8691bc2dc1430510af9e0341f4395a986185743
jebbwebb <98522086+jebbwebb@users.noreply.github.com>
2023-01-29 17:09:56 -0600

    added the correct anchor tags for my naviagtion and fixed some bugs with the hamburger menu

--- main.js
+++ main.js
@@ -1,12 +1,21 @@
 const stickyNav = document.querySelector('.container');
 const hamburger = document.querySelector('.hamburger');
 const mobileNav = document.querySelector('.mobile-nav');
+const mobileLinks = document.querySelectorAll('.mobile-nav a');
 
-console.log(stickyNav);
+console.log(mobileLinks);
 hamburger.addEventListener('click', function () {
   this.classList.toggle('is-active');
   mobileNav.classList.toggle('is-active');
 });
+
+mobileLinks.forEach((element) =>
+  element.addEventListener('click', () => {
+    console.log(mobileLinks);
+    mobileNav.classList.remove('is-active');
+    hamburger.classList.remove('is-active');
+  })
+);
 
 window.addEventListener('scroll', () => {
   if (window.pageYOffset >= 250) {

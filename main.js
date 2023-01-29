67de35c840398016dac0663106fac28994fd5e89
jebbwebb <98522086+jebbwebb@users.noreply.github.com>
2023-01-29 00:01:24 -0600

    added a sticky feature for my navbar when the page is scrolled down

--- main.js
+++ main.js
@@ -1,5 +1,16 @@
+const heightOutput = document.querySelector('#height');
+const stickyNav = document.querySelector('.container');
 const hamburger = document.querySelector('.hamburger');
-const landingContainer = document.querySelector('.landing-header');
+console.log(stickyNav);
+hamburger.addEventListener('click', function () {
+  this.classList.toggle('is-active');
+});
 
-console.log(landingContainer);
-console.log(widthOutput);
+window.addEventListener('scroll', () => {
+  if (window.pageYOffset >= 250) {
+    stickyNav.classList.add('sticky');
+  }
+  if (window.pageYOffset === 0) {
+    stickyNav.classList.remove('sticky');
+  }
+});

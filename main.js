105a347b98ae7a0066caba32865726640f6baaf5
jebbwebb <98522086+jebbwebb@users.noreply.github.com>
2023-01-28 21:59:37 -0600

    decided the landing page typing animation would require to much filler code to get it to work with all the constant line breaking so im going to go with another animation. Styled most of the fonts with the correct font-family and colors

--- main.js
+++ main.js
@@ -1,4 +1,5 @@
 const hamburger = document.querySelector('.hamburger');
-hamburger.addEventListener('click', function () {
-  this.classList.toggle('is-active');
-});
+const landingContainer = document.querySelector('.landing-header');
+
+console.log(landingContainer);
+console.log(widthOutput);

45285e72407c74b6b394f03e7ab2c57aade0e394
jebbwebb <98522086+jebbwebb@users.noreply.github.com>
2023-05-30 18:01:53 -0500

    changed my entire projects layout

--- main.js
+++ main.js
@@ -1,26 +1,54 @@
-const stickyNav = document.querySelector('.container');
-const hamburger = document.querySelector('.hamburger');
-const mobileNav = document.querySelector('.mobile-nav');
-const mobileLinks = document.querySelectorAll('.mobile-nav a');
+document.addEventListener('DOMContentLoaded', function () {
+  const stickyNav = document.querySelector('.container');
+  const hamburger = document.querySelector('.hamburger');
+  const mobileNav = document.querySelector('.mobile-nav');
+  const mobileLinks = document.querySelectorAll('.mobile-nav a');
+  const buttons = document.querySelectorAll('.yo');
+  const cards = document.querySelectorAll('.card');
 
-console.log(mobileLinks);
-hamburger.addEventListener('click', function () {
-  this.classList.toggle('is-active');
-  mobileNav.classList.toggle('is-active');
-});
+  buttons.forEach((button) => {
+    button.addEventListener('click', (event) => {
+      event.stopPropagation();
+    });
+  });
 
-mobileLinks.forEach((element) =>
-  element.addEventListener('click', () => {
-    mobileNav.classList.remove('is-active');
-    hamburger.classList.remove('is-active');
-  })
-);
+  const toggleFlipped = (event) => {
+    const card = event.currentTarget;
+    card.classList.toggle('flipped');
+  };
+  cards.forEach((card) => {
+    card.addEventListener('click', toggleFlipped);
+  });
 
-window.addEventListener('scroll', () => {
-  if (window.pageYOffset >= 250) {
-    stickyNav.classList.add('sticky');
-  }
-  if (window.pageYOffset === 0) {
-    stickyNav.classList.remove('sticky');
-  }
+  const removeFlippedOnSmallScreen = () => {
+    if (window.innerWidth < 1050) {
+      cards.forEach((card) => {
+        card.classList.remove('flipped');
+      });
+    }
+  };
+
+  hamburger.addEventListener('click', function () {
+    this.classList.toggle('is-active');
+    mobileNav.classList.toggle('is-active');
+  });
+
+  mobileLinks.forEach((element) =>
+    element.addEventListener('click', () => {
+      mobileNav.classList.remove('is-active');
+      hamburger.classList.remove('is-active');
+    })
+  );
+
+  window.addEventListener('scroll', () => {
+    if (window.pageYOffset >= 250) {
+      stickyNav.classList.add('sticky');
+    }
+    if (window.pageYOffset === 0) {
+      stickyNav.classList.remove('sticky');
+    }
+  });
+
+  removeFlippedOnSmallScreen();
+  window.addEventListener('resize', removeFlippedOnSmallScreen);
 });
